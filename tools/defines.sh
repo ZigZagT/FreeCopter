fd2867dfd802badd6d53009dcfa37cfc2b39a4e1
BananaWanted <i@BananaWanted.com>
2015-06-28 04:50:19 +0800

    back to xcode
    set up remote build and debug scripts with Xcode.

---
+++ tools/defines.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#  defines.sh
+#  Free-Copter
+#
+#  Created by Master on 6/26/15.
+#
+
+export LogPath="./log"
+export RemoteDir="/root/FC"
---
+++ tools/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#  run.sh
+#  Free-Copter
+#
+#  Created by Master on 6/26/15.
+#
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $DIR/defines.sh
+ssh root@ocean.local -p 22 -i ~/.ssh/id_rsa "cd $RemoteDir/dest; ./a.out;"
