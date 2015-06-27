32f8ffdc636ef3e6f0d5b3061c1a252572e4c696
BananaWanted <i@BananaWanted.com>
2015-06-28 01:39:28 +0800

    update

---
+++ Makefile
@@ -0,0 +1,2 @@
+a.out: main.cpp
+	g++ -std=c++11 main.cpp
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  Free-Copter
+//
+//  Created by Master on 6/26/15.
+//
+//
+
+#include <iostream>
+
+int main(const int argc, const char** argv) {
+    std::cout << "Hello Copter" << std::endl;
+}
