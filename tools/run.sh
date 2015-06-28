a478752dadaccd3934483ccd0b4a479db430ec58
BigOrange <lightful2000@gmail.com>
2015-06-28 22:19:10 +0800

    build script update
    All general build script has completed, Makefile needs update to
    partial build.

--- tools/run.sh
+++ tools/run.sh
@@ -7,4 +7,5 @@
 #
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/defines.sh
-ssh root@ocean.local -p 22 -i ~/.ssh/id_rsa "cd $RemoteDir/dest; ./a.out;"
+
+ssh root@ocean.local -p 22 -i ~/.ssh/id_rsa "cd $RemoteDir/dest; if [ -f ./fc ]; then ./fc; fi"
