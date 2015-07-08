ea37cee05ea369c2cf74dbf4ee2a9337a1e481d6
BigOrange <lightful2000@gmail.com>
2015-07-08 18:41:16 +0800

    snapshot

--- tools/run.sh
+++ tools/run.sh
@@ -8,4 +8,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/defines.sh
 
-ssh root@ocean.local -p 22 -i ~/.ssh/id_rsa "cd $RemoteDir/dest; if [ -f ./fc ]; then ./fc; fi"
+# ssh root@ocean.local -p 22 -i ~/.ssh/id_rsa "cd $RemoteDir/dest; if [ -f ./fc ]; then ./fc; fi"
