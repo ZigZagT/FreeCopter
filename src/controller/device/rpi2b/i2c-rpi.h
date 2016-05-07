89aeaa29d25101035654cafd12b166e7642cb0bb
4Oranges <lightful2000@gmail.com>
2016-05-08 02:03:48 +0800

    fix for c++ linkage

--- src/controller/device/rpi2b/i2c-rpi.h
+++ src/controller/device/rpi2b/i2c-rpi.h
@@ -3,8 +3,16 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int rpi_i2c_open(const char* path, uint8_t addr);
 int rpi_i2c_read_byte(int file, uint8_t *data);
 int rpi_i2c_write_byte(int file, uint8_t data);
+
+#ifdef __cplusplus
+};
+#endif
 
 #endif
