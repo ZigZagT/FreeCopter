ea174c9723d813fdf0c57e0a63c1401a1c8019a1
BananaWanted <i@BananaWanted.com>
2016-05-08 00:40:15 +0800

    fix build error

--- src/wrapper/device/m0518sd2ae/wcp-wrapper.c
+++ src/wrapper/device/m0518sd2ae/wcp-wrapper.c
@@ -1,7 +1,9 @@
 //
 // Created by master on 5/7/16.
 //
+#include <string.h>
 #include <wcp-wrapper.h>
+#include "define.h"
 #include "i2c_rw.h"
 
 uint8_t fc_wcp_status_transfer = FREECOPTER_WCP_STATUS_TRANSFER_IDLE;
@@ -21,18 +23,20 @@ int fc_wcp_reset_transfer_status() {
     fc_wcp_status_transfer_progress = 0;
 
     fc_wcp_status_timeout_count = fc_wcp_status_timeout_reload;
+    return 0;
 }
 
 int fc_wcp_reset_channel_status() {
     uint32_t i;
-    for (i = 0; i < fc_wcp_status_channels.channel_n) {
+    for (i = 0; i < fc_wcp_status_channels.channel_n; ++i) {
         fc_wcp_status_channels.channel[i].signal_source = FREECOPTER_WCP_CHANNEL_SIGSOURCE_FORWARD;
     }
+    return 0;
 }
 int fc_wcp_update_channel_status(unsigned long recv_control) {
-    size_t size = sizeof(fc_wcp_status_channels);
-    return I2C_Get_Recv_Data(recv_control, &fc_wcp_status_channels, &size);
+    uint32_t size = sizeof(fc_wcp_status_channels);
     fc_wcp_status_channels_control_expires = fc_wcp_status_channels_control_expires_reload;
+    return I2C_Get_Recv_Data((I2C_RECV_T*)recv_control, (uint8_t*)&fc_wcp_status_channels, &size);
 }
 
 int fc_wcp_loop() {
