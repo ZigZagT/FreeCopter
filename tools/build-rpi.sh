f11cb56958f9213b613e82b3c33201caf3597055
BananaWanted <i@BananaWanted.com>
2016-05-07 11:18:55 -0700

    improve build script

--- tools/build-rpi.sh
+++ tools/build-rpi.sh
@@ -9,24 +9,24 @@ if [ ! -d build ]; then
     mkdir build
 fi
 
+printf "\n"
 echo "detect source: "
-
 SOURCE_C=$(find src/controller/device/rpi2b | grep '.c$')
 SOURCE_CPP=$(find src/controller/device/rpi2b | grep '.cpp$')
-
 printf '%s\n' "${SOURCE_C[@]}"
 printf '%s\n' "${SOURCE_CPP[@]}"
+printf "\n"
 
 echo "use include directory: "
-
 INCLUDE="$PROJECT_DIR/src/include $PROJECT_DIR/src/controller/wcp $PROJECT_DIR/src/controller/device/rpi2b"
-
 printf '%s\n' "${INCLUDE[@]}"
-
-BUILD="gcc $SOURCE_C $SOURCE_CPP -o build/simpletest"
-
+printf "\n"
+BUILD="gcc -o build/simpletest"
 for d in $INCLUDE; do
-    BUILD="$BUILD -I$d"
+    BUILD="$BUILD -I $d"
 done
-
+for f in $SOURCE_C $SOURCE_CPP; do
+    BUILD="$BUILD $f"
+done
+printf "build with command: \n%s\n\n" "$BUILD"
 $BUILD
