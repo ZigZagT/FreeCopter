60a11026604a0b758e72b096b4a00b3df3f53a03
4Oranges <lightful2000@gmail.com>
2016-05-08 22:23:39 +0800

    backup changes

--- src/controller/device/rpi2b/test/simpletest.cpp
+++ src/controller/device/rpi2b/test/simpletest.cpp
@@ -1,6 +1,7 @@
 #include <wcp-controller.h>
 #include <iostream>
 #include <stdio.h>
+#include <errno.h>
 
 #include "i2c-rpi.h"
 
@@ -10,7 +11,18 @@ void dump_status(FREECOPTER_WCP_STATUS_T* status) {
 
 int main(int argc, char** argv) {
     int fd = rpi_i2c_open("/dev/i2c-1", 0x15);
+    int res;
+    if (fd < 0) {
+        printf("open i2c device failed\n");
+        exit(1);
+    }
     FREECOPTER_WCP_STATUS_T * status;
-    fc_wcp_get_status(fd, status);
+    while(true) {
+        sleep(100);
+        res = fc_wcp_get_status(fd, status);
+        if (res != 0) {
+            printf("get status filed %d, errno %d, %s\n", res, errno, strerror(errno));
+        }
+    }
     return 0;
 }
